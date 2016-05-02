269533d10b8efff73a30e8f1d7f92c4c46e7093a
Kevin Lee <sprint_l@aliyun.com>
2016-05-03 01:29:46 +0800

    create index to preview all tasks

---
+++ Scripts/database.js
@@ -0,0 +1,100 @@
+var db = new Array();
+    var objR = {
+        '阶段一' : {
+            '任务一' : {
+                title : '零基础HTML编码',
+                url : 'http://ife2016-running.github.io/IFE2016/RunningCoder/stage1/task1/index.html'
+            },
+            '任务二' : {
+                title : '零基础HTML及CSS编码',
+                url : 'http://ife2016-running.github.io/IFE2016/RunningCoder/stage1/task2/index.html'
+            },
+            '任务三' : {
+                title : '三栏式布局',
+                url : 'http://ife2016-running.github.io/IFE2016/RunningCoder/stage1/task3/index-1.html'
+            },
+            '任务四' : {
+                title : '定位和居中问题',
+                url : 'http://ife2016-running.github.io/IFE2016/RunningCoder/stage1/task4/index-1.html'
+            },
+            '任务五' : {
+                title : '任务五：零基础HTML及CSS编码（二）',
+                url : 'http://ife2016-running.github.io/IFE2016/RunningCoder/stage1/task5/index.html'
+            },
+            '任务六' : {
+                title : '通过HTML及CSS模拟报纸排版',
+                url : 'http://ife2016-running.github.io/IFE2016/RunningCoder/stage1/task6/index.html'
+            },
+        },
+        '阶段二' : {
+            '任务一' : '',
+            '任务二' : '',
+            '任务三' : '',
+            '任务四' : '',
+            '任务五' : '',
+            '任务六' : '',
+        },
+        '阶段三' : {
+            '任务一' : '',
+            '任务二' : '',
+            '任务三' : '',
+            '任务四' : '',
+            '任务五' : '',
+            '任务六' : '',
+        },
+        '阶段四': {
+            '任务一' : '',
+            '任务二' : '',
+            '任务三' : '',
+            '任务四' : '',
+            '任务五' : '',
+            '任务六' : '',
+        },
+    };
+    var objY = {
+        '阶段一' : {
+            '任务一' : {
+                title : '零基础HTML编码',
+                url : 'http://ife2016-running.github.io/IFE2016/YoToYang/stage1/task1/task1.html'
+            },
+            '任务二' : {
+                title : '零基础HTML及CSS编码',
+                url : 'http://ife2016-running.github.io/IFE2016/YoToYang/stage1/task2/task2.html'
+            },
+            '任务三' : {
+                title : '三栏式布局',
+                url : 'http://ife2016-running.github.io/IFE2016/YoToYang/stage1/task3/task3圣杯布局.html'
+            },
+            '任务四' : {
+                title : '定位和居中问题',
+                url : 'http://ife2016-running.github.io/IFE2016/RunningCoder/stage1/task4/task4.html'
+            },
+            '任务五' : '',
+            '任务六' : ''
+        },
+        '阶段二' : {
+            '任务一' : '',
+            '任务二' : '',
+            '任务三' : '',
+            '任务四' : '',
+            '任务五' : '',
+            '任务六' : ''
+        },
+        '阶段三' : {
+            '任务一' : '',
+            '任务二' : '',
+            '任务三' : '',
+            '任务四' : '',
+            '任务五' : '',
+            '任务六' : ''
+        },
+        '阶段四' : {
+            '任务一' : '',
+            '任务二' : '',
+            '任务三' : '',
+            '任务四' : '',
+            '任务五' : '',
+            '任务六' : ''
+        },
+    };
+db.push(objR, objY);
